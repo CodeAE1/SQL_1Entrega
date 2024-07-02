2aa00151af2e6726cbc0206ec63acdd988336ff5
CodeAE1 <145734912+CodeAE1@users.noreply.github.com>
2024-07-02 18:27:00 -0300

    Add files via upload

---
+++ Primera Entrega SQL.sql
@@ -0,0 +1,56 @@
+
+DROP DATABASE IF EXISTS proyecto_autos ;
+
+CREATE DATABASE proyecto_autos;
+
+USE proyecto_autos;
+
+
+-- Tabla Clientes
+CREATE TABLE Clientes (
+    ClienteID INT PRIMARY KEY AUTO_INCREMENT,
+    Nombre VARCHAR(50),
+    Apellido VARCHAR(50),
+    Email VARCHAR(100),
+    Telefono VARCHAR(20)
+);
+
+-- Tabla Proveedores
+CREATE TABLE Proveedores (
+    ProveedorID INT PRIMARY KEY AUTO_INCREMENT,
+    Nombre VARCHAR(100),
+    Direccion VARCHAR(200),
+    Telefono VARCHAR(20)
+);
+
+-- Tabla Vehículos
+CREATE TABLE Vehiculos (
+    VehiculoID INT PRIMARY KEY AUTO_INCREMENT,
+    Marca VARCHAR(50),
+    Modelo VARCHAR(50),
+    Año INT,
+    Precio DECIMAL(10, 2),
+    ProveedorID INT,
+    FOREIGN KEY (ProveedorID) REFERENCES Proveedores(ProveedorID)
+);
+
+-- Tabla Empleados
+CREATE TABLE Empleados (
+    EmpleadoID INT PRIMARY KEY AUTO_INCREMENT,
+    Nombre VARCHAR(50),
+    Apellido VARCHAR(50),
+    Cargo VARCHAR(50)
+);
+
+-- Tabla Ventas
+CREATE TABLE Ventas (
+    VentaID INT PRIMARY KEY AUTO_INCREMENT,
+    Fecha DATE,
+    VehiculoID INT,
+    ClienteID INT,
+    EmpleadoID INT,
+    PrecioVenta DECIMAL(10, 2),
+    FOREIGN KEY (VehiculoID) REFERENCES Vehiculos(VehiculoID),
+    FOREIGN KEY (ClienteID) REFERENCES Clientes(ClienteID),
+    FOREIGN KEY (EmpleadoID) REFERENCES Empleados(EmpleadoID)
+);
